7025ef6476a841b13dd63344e2f3124f9fa271d3
martamartiin <martamartin811@gmail.com>
2019-01-23 18:09:17 +0100

    just testing...

--- session2/exercise4.py
+++ session2/exercise4.py
@@ -1,5 +1,8 @@
- seq = imput("Introduce the sequence:")
+ seq = input("Introduce the sequence:")
  len = len(seq)
  print( "The length is :", len)
- bases_numb = seq.count()
+ print( "A:",seq.count("A"))
+ print( "G:",seq.count("G"))
+ print( "T:",seq.count("T"))
+ print( "C:",seq.count("C"))
 
